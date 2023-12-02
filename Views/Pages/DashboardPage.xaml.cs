b7d9f164665db1080deef09a379fcbe8fa0af94f
unknown <355188703@qq.com>
2023-12-02 22:07:07 +0800

    提交文件

---
+++ Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using MinecraftLaunch.Launch;
+using MinecraftLaunch.Modules.Models.Launch;
+using MinecraftLaunch.Modules.Utilities;
+using MinecraftOAuth.Authenticator;
+using System.Diagnostics;
+using Wpf.Ui.Controls;
+using ZMCL_neao.ViewModels.Pages;
+
+namespace ZMCL_neao.Views.Pages
+{
+    public partial class DashboardPage : INavigableView<DashboardViewModel>
+    {
+        public DashboardViewModel ViewModel { get; }
+
+        public DashboardPage(DashboardViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+            javalist.ItemsSource = JavaUtil.GetJavas();
+            gamelist.ItemsSource = new GameCoreUtil(".minecraft").GetGameCores();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OfflineAuthenticator authenticator = new(offlineplayerid.Text);
+            GameCoreUtil gcu = new(".minecraft"); //默认为.minecraft
+            var lc = new LaunchConfig(authenticator.Auth(), new(javalist.Text + "javaw.exe"));
+            var launcher = new JavaMinecraftLauncher(lc, new GameCoreUtil(".minecraft"));
+            JavaMinecraftLauncher jml = new(lc, gcu);
+            await launcher.LaunchTaskAsync(gamelist.Text, x =>
+            {
+                Debug.WriteLine(x.Item2);
+            });
+        }
+
+    }
+}
---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Wpf.Ui.Controls;
+using ZMCL_neao.ViewModels.Windows;
+using MinecraftLaunch.Launch;
+using MinecraftLaunch.Modules.Models.Launch;
+using MinecraftLaunch.Modules.Utilities;
+using MinecraftOAuth.Authenticator;
+
+namespace ZMCL_neao.Views.Windows
+{
+    public partial class MainWindow
+    {
+        public MainWindowViewModel ViewModel { get; }
+
+        public MainWindow(
+            MainWindowViewModel viewModel,
+            INavigationService navigationService,
+            IServiceProvider serviceProvider,
+            ISnackbarService snackbarService,
+            IContentDialogService contentDialogService
+        )
+        {
+            Wpf.Ui.Appearance.Watcher.Watch(this);
+
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+
+            navigationService.SetNavigationControl(NavigationView);
+            snackbarService.SetSnackbarPresenter(SnackbarPresenter);
+            contentDialogService.SetContentPresenter(RootContentDialog);
+
+            NavigationView.SetServiceProvider(serviceProvider);
+        }
+    }
+}
